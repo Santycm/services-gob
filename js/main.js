bb702011a69af335c2a48373c1fd132abd9b410c
Santycm <santicmoreno@gmail.com>
2024-07-10 22:01:21 -0500

    fix bug

--- js/main.js
+++ js/main.js
@@ -4,20 +4,20 @@ const loadServices = async () => {
   return data;
 };
 
- window.addEventListener("load", async () => {
+window.addEventListener("load", async () => {
   const services = await loadServices();
   services.forEach((item) => {
     if (item.id === 0) {
       item.ts.map((service) => {
         createCardS1(service);
       });
-    }else{
-        item.ts.map((service) => {
-          createCardS2(service);
-        });
+    } else {
+      item.ts.map((service) => {
+        createCardS2(service);
+      });
     }
   });
-}); 
+});
 
 const cards = document.getElementById("cards");
 
@@ -58,34 +58,6 @@ const createCardS2 = (service) => {
             </section>
             `;
   cards.appendChild(card);
-
-  /* 
-
-    let cardColor = document.createElement("DIV");
-
-    let cardContent = document.createElement("SECTION");
-    cardContent.setAttribute(
-      "class",
-      "py-[20px] px-[10px] grid gap-[10px] w-full"
-    );
-
-    let cardTitle = document.createElement("H3");
-    cardTitle.setAttribute(
-      "class",
-      "text-[15px] line-clamp-3 font-semibold leading-tight"
-    );
-
-    let cardSection = document.createElement("SECTION");
-    cardSection.setAttribute("flex items-center gap-[10px]");
-
-    let btnCard = document.createElement("")
-
-    if(item.id===0){
-        cardColor.setAttribute(
-          "class",
-          "h-full w-[12px] bg-principal group-hover:bg-hoverPrincipal transition-colors duration-500"
-        );
-    } */
 };
 
 const searchInput = document.getElementById("searchInput");
@@ -93,7 +65,7 @@ const searchInput = document.getElementById("searchInput");
 const searchService = async (word) => {
   const response = await fetch("./js/serv-tram.json");
   const data = await response.json();
-  data.map((item) =>
+  data.forEach((item) => {
     item.ts.forEach((service) => {
       if (service.title.toLowerCase().includes(word.toLocaleLowerCase())) {
         if (item.id === 0) {
@@ -102,19 +74,12 @@ const searchService = async (word) => {
           createCardS2(service);
         }
       }
-    })
-  );
+    });
+  });
 };
 
 searchInput.addEventListener("input", () => {
-    if(cards.hasChildNodes){
-        let listCards = document.querySelectorAll("#cards > article");
-        listCards.forEach((element)=>{
-            element.remove();
-        });
-        searchService(searchInput.value)
-    }else{
-        searchService(searchInput.value)
-    }
+  cards.innerHTML = '';
+  searchService(searchInput.value);
+  
 });
-
