f88339e27fed6cddfe0a05f6206aa5b9d7facd1d
Santycm <santicmoreno@gmail.com>
2024-07-11 16:15:02 -0500

    finish design and functions

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ const loadServices = async () => {
 
 window.addEventListener("load", async () => {
   const services = await loadServices();
+  loadAll(services)
+});
+
+const loadAll = (services)=>{
   services.forEach((item) => {
     if (item.id === 0) {
       item.ts.map((service) => {
@@ -17,7 +21,7 @@ window.addEventListener("load", async () => {
       });
     }
   });
-});
+}
 
 const cards = document.getElementById("cards");
 
@@ -26,15 +30,15 @@ const createCardS1 = (service) => {
   let card = document.createElement("ARTICLE");
   card.setAttribute(
     "class",
-    "flex border-[1px] border-gray-300 rounded-lg overflow-hidden hover:shadow-lg transition-all duration-500 group"
+    "relative flex border-[1px] border-gray-300 rounded-lg overflow-hidden hover:shadow-lg transition-all duration-500 group"
   );
   card.innerHTML = `
-        <div class="h-full w-[12px] bg-principal group-hover:bg-hoverPrincipal transition-colors duration-500"></div>
-            <section class="py-[20px] px-[10px] grid gap-[10px] w-full">
-                <h3 class="text-[15px] line-clamp-3 font-semibold leading-tight">${service.title}</h3>
+        <div class="h-full w-[12px] group-hover:w-full absolute bg-principal transition-colors duration-700 transition-all"></div>
+            <section class="py-[20px] px-[10px] grid gap-[10px] w-full relative ml-[12px]">
+                <h3 class="text-[15px] line-clamp-2 font-semibold leading-tight group-hover:text-white">${service.title}</h3>
                     <section class="flex items-center gap-[10px]">
-                        <a href="${service.btn}" class="text-[10px] font-semibold bg-principal hover:bg-hoverPrincipal  text-white px-[10px] py-[6px] rounded-full">REALIZAR</a>
-                        <a href="${service.link}" class="text-[12px]">Conoce más</a>
+                        <a href="${service.btn}" class="text-[10px] font-semibold bg-principal hover:bg-principal group-hover:bg-white group-hover:text-black text-white px-[10px] py-[6px] rounded-full">REALIZAR</a>
+                        <a href="${service.link}" class="text-[12px] group-hover:text-white hover:font-bold">Conoce más</a>
                     </section>
             </section>
             `;
@@ -45,15 +49,15 @@ const createCardS2 = (service) => {
   let card = document.createElement("ARTICLE");
   card.setAttribute(
     "class",
-    "flex border-[1px] border-gray-300 rounded-lg overflow-hidden hover:shadow-lg transition-all duration-500 group"
+    "relative flex border-[1px] border-gray-300 rounded-lg overflow-hidden hover:shadow-lg transition-all duration-500 group"
   );
   card.innerHTML = `
-        <div class="h-full w-[12px] bg-second group-hover:bg-hoverSecond transition-colors duration-500"></div>
-            <section class="py-[20px] px-[10px] grid gap-[10px] w-full">
-                <h3 class="text-[15px] line-clamp-3 font-semibold leading-tight">${service.title}</h3>
+        <div class="h-full w-[12px] group-hover:w-full absolute bg-second transition-colors duration-700 transition-all"></div>
+            <section class="py-[20px] px-[10px] grid gap-[10px] w-full relative ml-[12px]">
+                <h3 class="text-[15px] line-clamp-2 font-semibold leading-tight">${service.title}</h3>
                     <section class="flex items-center gap-[10px]">
-                        <a href="${service.btn}" class="text-[10px] font-semibold bg-second hover:bg-hoverSecond  text-white px-[10px] py-[6px] rounded-full">REALIZAR</a>
-                        <a href="${service.link}" class="text-[12px]">Conoce más</a>
+                        <a href="${service.btn}" class="text-[10px] font-semibold bg-second group-hover:bg-white group-hover:text-black text-white hover:font-bold px-[10px] py-[6px] rounded-full">REALIZAR</a>
+                        <a href="${service.link}" class="text-[12px] hover:font-bold">Conoce más</a>
                     </section>
             </section>
             `;
@@ -77,7 +81,6 @@ const searchService = async (word) => {
     });
   });
 
-  console.log("------------------")
   for(let i=0; i<results.length; i++){
     if(results[i].secret === "salud"){
       createCardS1(results[i])
@@ -89,6 +92,35 @@ const searchService = async (word) => {
 };
 
 searchInput.addEventListener("input", function(){
-  
   searchService(searchInput.value);
 });
+
+searchInput.addEventListener("keydown", (e)=>{
+  if(e.key==="Enter"){
+    viewResults();
+  }
+})
+
+function viewResults(){
+  cards.scrollIntoView({behavior:'smooth'})
+}
+
+const filterResult = async(filter)=>{
+  const services = await loadServices();
+  cards.innerHTML = "";
+  services[filter].ts.forEach((item)=>{
+    if(filter===0){
+      createCardS1(item)
+    }else{
+      createCardS2(item)
+    }
+  })
+  document.getElementById("buttonShowAll").classList.remove("hidden");
+}
+
+const showAll = async()=>{
+  cards.innerHTML = "";
+  document.getElementById("buttonShowAll").classList.add("hidden");
+  const services = await loadServices();
+  loadAll(services);
+}
--- js/tailwindConfig.js
+++ js/tailwindConfig.js
@@ -4,10 +4,13 @@ tailwind.config = {
     extend: {
       colors: {
         principal: "#6d0205",
-        second: "#c79500",
+        second: "#f9b90e",
+        third: "#edecec",
 
         hoverPrincipal: "#b90308",
-        hoverSecond: "#fbbc00",
+        hoverSecond: "#c5930b",
+
+        txtSecond: "#b18630",
       },
     },
   },
